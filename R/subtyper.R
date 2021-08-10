3a3dc8c0475d1c55a4aa5ebe105273f4ac7dc257
stnava <stnava@gmail.com>
2021-08-10 07:36:00 -0400

    Initial commit of package skeleton

---
+++ R/subtyper.R
@@ -0,0 +1,6 @@
+# Here's a good place to put your top-level package documentation
+
+.onLoad <- function (lib, pkgname="subtyper") {
+    ## Put stuff here you want to run when your package is loaded
+    invisible()
+}
