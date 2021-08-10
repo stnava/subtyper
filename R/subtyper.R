d6a190d0e49d155fccd3d2e11ad82636ceeab186
stnava <stnava@gmail.com>
2021-08-10 11:59:16 -0400

    ENH: ....

--- R/subtyper.R
+++ R/subtyper.R
@@ -80,6 +80,7 @@ generateSubtyperData <-function( n = 100 ) {
 #'
 #' @param mxdfin Input data frame with repeated measurements and a grouped time variable
 #' @param idvar variable name for unique subject identifier column
+#' @param measurement the numeric variable to plot over time stratified by subtype
 #' @param subtype the categorical (possibly ordinal) subtype variable
 #' @param vizname the name of the grouped time variable (e.g. years change rounded to nearest quarter year)
 #' @param whiskervar character either ci or se
@@ -93,9 +94,14 @@ generateSubtyperData <-function( n = 100 ) {
 #' mydf = generateSubtyperData( 100 )
 #' summ = plotSubtypeChange( mydf, "Id", "cognition", "DX", "visit" )
 #' @export
+#' @importFrom stats lm predict qt rnorm var
+#' @importFrom DDoutlier  LOOP  LOF  INFLO  RDOS  KDEOS  LDF  KNN_AGG  KNN_IN  KNN_SUM  RKOF
+#' @importFrom ggplot2 aes ylim guides theme_bw scale_colour_hue geom_errorbar position_dodge element_text geom_line geom_point ggplot guide_legend
+#' @importFrom ggplot2 xlab ylab theme rel
+#' @importFrom plyr ddply rename
 plotSubtypeChange <-function( mxdfin,
                            idvar,
-                           msr1,
+                           measurement,
                            subtype,
                            vizname='timer', # should be roundable to integer
                            whiskervar = 'ci',
@@ -112,8 +118,6 @@ plotSubtypeChange <-function( mxdfin,
 
       summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=TRUE,
                             conf.interval=.95, .drop=TRUE) {
-          library(plyr)
-
           # New version of length which can handle NA's: if na.rm==T, don't count them
           length2 <- function (x, na.rm=FALSE) {
               if (na.rm) sum(!is.na(x))
@@ -150,7 +154,7 @@ plotSubtypeChange <-function( mxdfin,
     lmxdf$timer = lmxdf[ , vizname ]
 # When the population standard deviation is known, the formula for a confidence interval (CI) for a population mean is x̄ ± z* σ/√n, where x̄ is the sample mean, σ is the population standard deviation, n is the sample size, and z* represents the appropriate z*-value from the standard normal distribution for your desired confidence level.
 
-    tgcWithin <- summarySE( lmxdf, measurevar=msr1,
+    tgcWithin <- summarySE( lmxdf, measurevar=measurement,
             groupvars=c('timer',subtype), na.rm=TRUE, conf.interval=.95 )
 
     colnames( tgcWithin )[1:5] = c('timer',"Quant","N","Changer","Changer_norm")
@@ -159,13 +163,13 @@ plotSubtypeChange <-function( mxdfin,
     yran = range( c( tgcWithin$ymin,tgcWithin$ymax ) )
     pd <- position_dodge( 0.05 ) # move them .05 to the left and right
     ww = 0.5
-    namer=paste( subtype, ' vs ', msr1 , extra )
+    namer=paste( subtype, ' vs ', measurement , extra )
     print( ggplot( tgcWithin,
       aes(x=timer, y=Changer, group = Quant, color = Quant , shape = Quant )) +
       geom_errorbar(aes(ymin=ymin, ymax=ymax), colour="black", width=ww, position=pd) +
       geom_line(lwd = 1, show.legend = FALSE ) +
       geom_point(position=pd, size=3, shape=21, fill="white") + # 21 is filled circle
-      xlab( xlab ) + ylab( paste(ylab, msr1," +/-", whiskervar )) +
+      xlab( xlab ) + ylab( paste(ylab, measurement," +/-", whiskervar )) +
       ylim(yran[1], yran[2]) +
       theme(strip.text = element_text(size = rel(1.1))) +
       theme(legend.position = 'top') +
@@ -191,8 +195,8 @@ plotSubtypeChange <-function( mxdfin,
 #' @return data frame
 #' @author Avants BB
 #' @examples
-#'
-#'
+#' mydf = generateSubtyperData( 100 )
+#' mydf = filterByVisit( mydf,  "visit", c("V0","V1"))
 #' @export
 filterByVisit  <-function(
   mxdfin,
@@ -250,11 +254,9 @@ highestQualityRepeat  <-function(
 
 
 
-#' Get subjects and timepoints with the best quality
+#' Outlierness scoring
 #'
-#' This filters data that has both repeats (e.g. multiple images on the same day)
-#' and (optionally) longitudinal data.  The quality measure should be a "higher"
-#' is better" measurement.
+#' Produce several complementary measurements of outlierness
 #'
 #' @param mxdfin Input data frame with repeated measurements and a grouped time variable
 #' @param measureColumns vector string of column names to use in outlier detection
@@ -290,15 +292,103 @@ outlierness <- function( mxdfin, measureColumns, calck, report=FALSE ) {
   return( mxdfin )
 }
 
-filterByQuality  <- function( ) {}  # strategies to identify a high quality subset of data
-
-
-adjustByCovariates  <- function( ) {}  # adjust by nuisance variables eg field strength etc
+# filterByQuality  <- function( ) {}  # strategies to identify a high quality subset of data
 
 
 
-trainSubtype  <- function( ) {}  # training module for subtype definition
+#' Covariate adjustment
+#'
+#' Adjust a training vector value by nuisance variables eg field strength etc.
+#' One may want to use a specific sub-group for this, e.g. controls.
+#'
+#' @param mxdfin Input data frame with repeated measurements and a grouped time variable
+#' @param adjustmentFormula string defining a valid formula to be used in \code{lm}.
+#' @param groupVariable names of the column that defines the group to use for training.
+#' @param group string defining a subgroup on which to train
+#' @return data frame with adjusted measurement variable as defined in formula
+#' @author Avants BB
+#' @examples
+#' mydf = generateSubtyperData( 100 )
+#' myform = "cognition ~ RandomBasisProjection01 "
+#' mydf = adjustByCovariates(mydf,myform,"DX","G0")
+#' @export
+adjustByCovariates  <- function(
+  mxdfin,
+  adjustmentFormula,
+  groupVariable,
+  group
+) {
+  outcomevar = gsub( " ", "", unlist(strsplit( adjustmentFormula, "~" ))[[1]] )
+  if ( ! missing( group ) & ! missing( groupVariable ) ) {
+    if ( ! (groupVariable %in% names( mxdfin ) ) ) stop("group name is wrong")
+    gsel = mxdfin[,groupVariable] == group
+    if ( sum(gsel) < 5 ) stop("too few subjects in subgroup for training")
+    subdf = mxdfin[ gsel, ]
+  } else subdf = mxdfin
+  baseform = paste( outcomevar, " ~ 1" )
+  imodel = lm( baseform, data=subdf ) # just the intercept
+  ctlmodel = lm( adjustmentFormula, data=subdf )
+  predvol = predict( ctlmodel, newdata = mxdfin ) - predict( imodel, newdata = mxdfin  )
+  adjustedoutcome = paste0( outcomevar, "_adjusted" )
+  mxdfin[ , adjustedoutcome ] = mxdfin[,outcomevar] - predvol
+  return( mxdfin )
+}
 
 
+#' Train subtype for univariate data
+#'
+#' This is the training module for subtype definition based on a vector.
+#' Produces a data frame denoting the measurement variable, its cutpoints and
+#' the names of the subtypes.
+#' One may want to use a specific sub-group for this, e.g. patients.
+#'
+#' @param mxdfin Input data frame
+#' @param measureColumn string defining a valid column to use for subtyping.  E.g.
+#' the output of the covariate adjustment done by \code{adjustByCovariates}.
+#' @param quantiles numeric vector defining the quantiles that will yield subgroups
+#' @param groupVariable names of the column that defines the group to use for training.
+#' @param group string defining a subgroup on which to train
+#' @return data frame defining the subtypes and cutpoints
+#' @author Avants BB
+#' @examples
+#' mydf = generateSubtyperData( 100 )
+#' @export
+trainSubtypeUni  <- function(
+  mxdfin,
+  measureColumn,
+  quantiles = c(0.25,0.75),
+  groupVariable,
+  group
+) {
 
-predictSubtype  <- function( ) {} # inference module for subtype definition
+  if ( ! missing( group ) & ! missing( groupVariable ) ) {
+    if ( ! (groupVariable %in% names( mxdfin ) ) ) stop("group name is wrong")
+    gsel = mxdfin[,groupVariable] == group
+    if ( sum(gsel) < 5 ) stop("too few subjects in subgroup for training")
+    subdf = mxdfin[ gsel, ]
+  } else {
+    subdf = mxdfin
+    group=NA
+  }
+
+  stdf = data.frame( )
+  stdf
+}
+
+
+#' Predict subtype for univariate data
+#'
+#' This is the inference module for subtype definition based on a vector.
+#' After training, we can predict subtype very easily in new data based on
+#' the data frame produced by training \code{trainSubtypeUni}.
+#'
+#' @param mxdfin Input data frame
+#' @param subtypeDataFrame data frame defining the subtypes and cutpoints
+#' @return data frame with attached subtypess
+#' @author Avants BB
+#' @examples
+#' mydf = generateSubtyperData( 100 )
+#' @export
+predictSubtypeUni  <- function( mxdfin, subtypeDataFrame ) {
+
+}
