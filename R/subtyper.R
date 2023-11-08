33dee43621ece7de0c931928acc78c1a8fa706f6
stnava <stnava@gmail.com>
2023-11-08 14:08:12 -0500

    ENH: coca refinement

--- R/subtyper.R
+++ R/subtyper.R
@@ -3132,6 +3132,7 @@ consensusSubtypingPredict = function( dataToClust, featureNames, clustVec, clust
 #' @param idvar variable name for unique subject identifier column
 #' @param visitName the column name defining the visit variables
 #' @param baselineVisit the string naming the baseline visit
+#' @param maxK maximum number of clusters
 #' @param consensusmethod either kmeans or hclust
 #' @param verbose boolean
 #' @return new dataframe with new variables attached
@@ -3140,7 +3141,7 @@ consensusSubtypingPredict = function( dataToClust, featureNames, clustVec, clust
 #' mydf = generateSubtyperData( 100 )
 #' @importFrom caret dummyVars contr.ltfr
 #' @export
-consensusSubtypingCOCA = function( dataToClust, targetk, cocanames, newclustername, reorderingVariable, idvar, visitName, baselineVisit, consensusmethod='kmeans',verbose=TRUE ) {
+consensusSubtypingCOCA = function( dataToClust, targetk, cocanames, newclustername, reorderingVariable, idvar, visitName, baselineVisit, maxK, consensusmethod='kmeans',verbose=TRUE ) {
     # assume we already ran consensuscluster
     if ( !missing(idvar) )
       stopifnot( idvar %in% colnames(dataToClust) )
@@ -3171,7 +3172,15 @@ consensusSubtypingCOCA = function( dataToClust, targetk, cocanames, newclusterna
     }
     dmy = dummyVars(cocoform, data = dataToClust[,cocanames])
     dmytx = data.frame(predict(dmy, newdata = dataToClust[isbl,cocanames]))
-    cocatx = coca::coca(dmytx, K = targetk, B=1000, maxIterKM=5000, ccClMethod=consensusmethod )
+    if ( ! missing( targetk ) & missing( maxK ) ) {
+      cocatx = coca::coca(dmytx, K = targetk, B=1000, maxIterKM=5000, ccClMethod=consensusmethod )
+    } else if ( ! missing( maxK ) ) {
+      message(paste(consensusmethod,'spearman',maxK))
+      cocatx = coca::coca(dmytx, K = NULL, maxK=maxK, 
+        # choiceKmethod='AUC', 
+        widestGap=TRUE, ccDistHC='spearman',
+        B=1000, maxIterKM=5000, ccClMethod=consensusmethod )
+    } else stop("Must set either maxK or targetk")
 #    cocatx = coca::coca(dmytx, maxK = 6, B=5000 )
 #    coca = coca::coca( dmytx, maxK = 10, hclustMethod = "average")
     cocatxlab = as.numeric( cocatx$clusterLabels )
