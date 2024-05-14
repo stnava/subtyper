fb91cc4812e5743a27943090524bb910122c0690
stnava <stnava@gmail.com>
2024-05-14 09:09:43 -0400

    DOC: brainstem stuff

--- R/subtyper.R
+++ R/subtyper.R
@@ -68,14 +68,17 @@ filterNAcolumns <- function(df, percentThreshold) {
 #' @importFrom dplyr filter
 #' @importFrom magrittr %>%
 antspymm_predictors <- function( demog, doasym=FALSE, return_colnames=FALSE ) {
+  t1namesbst = getNamesFromDataframe( c("T1Hier",'brainstem','vol'), demog, exclusions=c("tissues","lobes"))[-1]
   testnames=c(
           getNamesFromDataframe( "T1w_" , demog, exclusions=c("hier_id",'background','thk','area','vol','SNR','evr')),
           getNamesFromDataframe( "mtl" , demog, exclusions=c("hier_id",'background','thk','area','vol','SNR','evr')),
           getNamesFromDataframe( "cerebellum" , demog, exclusions=c("hier_id",'background','thk','area','vol','SNR','evr',"_cerebell")),
           getNamesFromDataframe( "T1Hier_" , demog, exclusions=c("hier_id","[.]1","[.]2","[.]3",'background','tissue','dktregions','T1Hier_resnetGrade','hemisphere','lobes','SNR','evr','area')),
+          t1namesbst,
           getNamesFromDataframe( "rsfMRI_fcnxpro" , demog, exclusions=c("hier_id",'background','thk','area','vol','FD','dvars','ssnr','tsnr','motion','SNR','evr','_alff','falff_sd','falff_mean')),
           getNamesFromDataframe( "perf_" , demog, exclusions=c("hier_id",'background','thk','area','vol','FD','dvars','ssnr','tsnr','motion','SNR','evr','_alff','falff_sd','falff_mean')),
           getNamesFromDataframe( "DTI_" , demog, exclusions=c("hier_id",'background','thk','area','vol','motion','FD','dvars','ssnr','tsnr','SNR','evr','cnx','relcn')) )
+  testnames = unique( testnames )
   if ( return_colnames ) return( testnames )
 
   if ( FALSE ) {
@@ -3097,6 +3100,7 @@ shorten_pymm_names <-function(x){
     xx=gsub("cit168",'',xx,fixed=TRUE)
     xx=gsub(".include",'',xx,fixed=TRUE)
     xx=gsub("mtg.sn",'',xx,fixed=TRUE)
+    xx=gsub("brainstem",'.bst',xx,fixed=TRUE)
     xx=gsub("rsfmri.fcnxpro122.",'rsf.',xx,fixed=TRUE)
     xx=gsub("dti.mean.fa.",'dti.fa.',xx,fixed=TRUE)
     xx=gsub("perf.cbf.mean.",'cbf.',xx,fixed=TRUE)
